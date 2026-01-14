a54cacdbf547a7127b19a9f8a972f552b25ff261
Thunder01-wow <devarthdarge70@gmail.com>
2026-01-14 16:43:07 +0530

    Time: 11 ms (74.67%) | Memory: 19.5 MB (10.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,4 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+        if len(s) != len(t):
+            return False
+        
+        s_dict={}
+        t_dict={}
+
+        for i in range(len(s)):
+            s_dict[s[i]]=1 + s_dict.get(s[i],0)
+            t_dict[t[i]]=1 + t_dict.get(t[i],0)
+        
+        return s_dict == t_dict
+
+
         
