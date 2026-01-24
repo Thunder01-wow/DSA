af4c1774ae81961c8b4724741ac8956d916cddae
Thunder01-wow <devarthdarge70@gmail.com>
2026-01-24 13:05:17 +0530

    Time: 107 ms (84.36%) | Memory: 27.7 MB (32.01%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        res=[]
+        heapq.heapify(nums)
+        while nums:
+            val=heapq.heappop(nums)
+            res.append(val)
+        
+        return res
+
+            
+
+        
