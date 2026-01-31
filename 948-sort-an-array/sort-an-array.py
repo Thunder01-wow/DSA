b618737a343f0ac13b137d1995eef73e184e1f02
Thunder01-wow <devarthdarge70@gmail.com>
2026-01-31 12:22:11 +0530

    Time: 774 ms (20.33%) | Memory: 26.5 MB (51.13%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,12 +1,41 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        res=[]
-        heapq.heapify(nums)
-        while nums:
-            val=heapq.heappop(nums)
-            res.append(val)
+
+        def merge(arr,L,Mid,R):
+            left=arr[L:Mid+1]
+            right=arr[Mid+1:R+1]
+            i,j,k=L,0,0
+
+            while j < len(left) and k < len(right):
+                if left[j] <= right[k]:
+                    arr[i] = left[j]
+                    j+=1
+                else:
+                    arr[i]=right[k]
+                    k+=1
+                i+=1
+            
+            while j < len(left):
+                arr[i] = left[j]
+                j+=1
+                i+=1
+            
+            while k < len(right):
+                arr[i] = right[k]
+                k+=1
+                i+=1
+            
+        def MergeSort(arr,l,r):
+            if l == r:
+                return arr
+            
+            mid = (l+r)//2
+            MergeSort(arr,l,mid)
+            MergeSort(arr,mid+1,r)
+            merge(arr,l,mid,r)
+            return arr
         
-        return res
+        return MergeSort(nums,0,len(nums)-1)
 
             
 
